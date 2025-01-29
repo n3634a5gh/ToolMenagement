85c01bcfeb1b0e571d7b77234527c413a248dc68
Mateusz <mateusz.cmok7@gmail.com>
2025-01-29 19:33:53 +0100

    Add published files

---
+++ Desktop/MobileAppTest/wwwroot/scripts/map.js
@@ -0,0 +1,133 @@
+﻿let currentMap = null;
+let currentUserMarker = null;
+let currentDirectionsRenderer = null;
+let currentWatchId = null;
+let currentIndex = 0;
+
+window.initializeMapWithLiveLocatio = function (points, dotnetInstance) {
+
+    var directionsService = new google.maps.DirectionsService();
+    var directionsRenderer = new google.maps.DirectionsRenderer();
+    var map, userMarker;
+    let visitedPoints = new Set();
+
+    // do odswieżania przy zmianie mapy
+    if (currentMap) {
+        currentDirectionsRenderer.setMap(null);
+        currentUserMarker.setMap(null);
+        currentDirectionsRenderer = null;
+        currentUserMarker = null;
+        currentMap = null;
+        if (currentWatchId !== null) {
+            navigator.geolocation.clearWatch(currentWatchId);
+            currentWatchId = null;
+        }
+    }
+
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(function (position)
+        {
+            var userLocation = {
+                lat: position.coords.latitude,
+                lng: position.coords.longitude
+            };
+
+            // nowa mapa
+            var mapOptions = {
+                center: userLocation,
+                zoom: 14
+            };
+
+            currentMap = new google.maps.Map(document.getElementById('map'), mapOptions);
+            currentDirectionsRenderer = new google.maps.DirectionsRenderer();
+            currentDirectionsRenderer.setMap(currentMap);
+
+            currentUserMarker = new google.maps.Marker(
+                {
+                position: userLocation,
+                map: currentMap,
+                title: "JA",
+                icon: {
+                    url: "http://maps.google.com/mapfiles/ms/icons/blue-dot.png"
+                }
+            });
+            points.forEach(point => {
+                new google.maps.Marker({
+                    position: { lat: point.latitude, lng: point.longitude },
+                    map: currentMap,
+                    title: point.name
+                });
+            });
+
+            function getDistance(lat1, lon1, lat2, lon2) {
+                const R = 6371000; // promień Ziemi w metrach
+                const fi1 = lat1 * Math.PI / 180;
+                const fi2 = lat2 * Math.PI / 180;
+                const Δφ = (lat2 - lat1) * Math.PI / 180;
+                const Δλ = (lon2 - lon1) * Math.PI / 180;
+
+                const a = Math.sin(Δφ / 2) * Math.sin(Δφ / 2) +
+                    Math.cos(fi1) * Math.cos(fi2) *
+                    Math.sin(Δλ / 2) * Math.sin(Δλ / 2);
+                const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+
+                return R * c;
+            }
+
+            function checkVisitedPoints(userLat, userLng)
+            {
+                if (currentIndex < points.length)
+                {
+                    const point = points[currentIndex];
+                    const distance = getDistance(userLat, userLng, point.latitude, point.longitude);
+                    if (distance <= 30)
+                    {
+                        visitedPoints.add(currentIndex);
+                        console.log(`Zaliczony: ${point.name}`);
+                        dotnetInstance.invokeMethodAsync("MarkPointAsVisited", currentIndex);
+                        currentIndex++;
+                    }
+                }
+            }
+
+            // wyznaczacz trase
+            function updateRoute(currentLocation) {
+                var waypoints = points.map(point => ({
+                    location: new google.maps.LatLng(point.latitude, point.longitude),
+                    stopover: true
+                }));
+
+                var request = {
+                    origin: currentLocation,
+                    destination: waypoints[waypoints.length - 1].location,
+                    waypoints: waypoints.slice(0, waypoints.length - 1),
+                    travelMode: google.maps.TravelMode.WALKING
+                };
+
+                directionsService.route(request, function (result, status) {
+                    if (status === google.maps.DirectionsStatus.OK) {
+                        currentDirectionsRenderer.setDirections(result);
+                    } else {
+                        console.error("Nie udało się wyznaczyć trasy: " + status);
+                    }
+                });
+            }
+
+            updateRoute(userLocation);
+            currentWatchId = navigator.geolocation.watchPosition(function (position) {
+                var newLocation = {
+                    lat: position.coords.latitude,
+                    lng: position.coords.longitude
+                };
+                currentUserMarker.setPosition(newLocation);
+                currentMap.setCenter(newLocation);
+                updateRoute(newLocation);
+
+                checkVisitedPoints(newLocation.lat, newLocation.lng);
+            });
+        }
+        );
+    } else {
+        alert("Błąd geolokalizacji.");
+    }
+};
---
+++ Desktop/MobileAppTest/wwwroot/service-worker-assets.js
@@ -0,0 +1,321 @@
+self.assetsManifest = {
+  "version": "8xF9aqwV",
+  "assets": [
+    {
+      "hash": "sha256-o5vij9DaPjEwn2HRMGpGQM/jiRxzV11IaycuXglRCAc=",
+      "url": "Mobilna_Gra_Terenowa.styles.css"
+    },
+    {
+      "hash": "sha256-vbmhR3SSHxQ7d2rRoVmpYplGgf5f0NGZK4xeg+ufpME=",
+      "url": "_framework/Microsoft.AspNetCore.Components.Web.wasm"
+    },
+    {
+      "hash": "sha256-ifXv4cvxY+tAadYQj0bb7NdDA3dA0Ze0OeLWnOzP/1Q=",
+      "url": "_framework/Microsoft.AspNetCore.Components.WebAssembly.wasm"
+    },
+    {
+      "hash": "sha256-Mca5kQWGfgGzRFBMEl8orI3x5q7Zbhq5aJjlCmvg3sw=",
+      "url": "_framework/Microsoft.AspNetCore.Components.wasm"
+    },
+    {
+      "hash": "sha256-EBu0K8SFe/KbDl6H4PhdQZcfOWBAQS9MiNm+0jjq0xo=",
+      "url": "_framework/Microsoft.AspNetCore.Hosting.Abstractions.wasm"
+    },
+    {
+      "hash": "sha256-KOdV8nnepJvdzz9YZQF2IiESUrXNx6nNMN2Uvhssmi4=",
+      "url": "_framework/Microsoft.AspNetCore.Hosting.Server.Abstractions.wasm"
+    },
+    {
+      "hash": "sha256-D5749pnNwpM2NuL1FU/fMqcIC9imznPeawWvirjCaX4=",
+      "url": "_framework/Microsoft.AspNetCore.Hosting.wasm"
+    },
+    {
+      "hash": "sha256-7wgz1ZHwXmXV8y00xn+jT4BGeuCQQbVYGQl+PNvBfnQ=",
+      "url": "_framework/Microsoft.AspNetCore.Http.Abstractions.wasm"
+    },
+    {
+      "hash": "sha256-sbIJEHnGL6std1HkIJ83mfNTk5yznrGRZbaacPDI5w4=",
+      "url": "_framework/Microsoft.AspNetCore.Http.Extensions.wasm"
+    },
+    {
+      "hash": "sha256-bzIPAVp+XhD63CRn9cVTB4tJRU+oDEAuwFYoHytMhUk=",
+      "url": "_framework/Microsoft.AspNetCore.Http.Features.wasm"
+    },
+    {
+      "hash": "sha256-P7u++4zIBAR6s/X8yOsyscat1H9SsWsnAnBNrm4OxlA=",
+      "url": "_framework/Microsoft.AspNetCore.Http.wasm"
+    },
+    {
+      "hash": "sha256-VQM0rTyQ42zNsi2Mfz/XlOZsi4F4HQ5RrZnGIJaRhjI=",
+      "url": "_framework/Microsoft.AspNetCore.WebUtilities.wasm"
+    },
+    {
+      "hash": "sha256-RyotBhMNRuC3SwWevVjUH4cNaEaIDMnO1kMz4hw2LUY=",
+      "url": "_framework/Microsoft.Extensions.Configuration.Abstractions.wasm"
+    },
+    {
+      "hash": "sha256-7xn1r3E7cQCCZAAbyfiMgbXsj6G0/niyLco/nyBy43g=",
+      "url": "_framework/Microsoft.Extensions.Configuration.EnvironmentVariables.wasm"
+    },
+    {
+      "hash": "sha256-taqXVMF0ENQlExieyTo9KvBRbMreQPeb+kqaF5mmc9s=",
+      "url": "_framework/Microsoft.Extensions.Configuration.FileExtensions.wasm"
+    },
+    {
+      "hash": "sha256-Nbv4DDWJLbUri+9QrX+3vnA2FV/Qux8Jm1buTARMlBs=",
+      "url": "_framework/Microsoft.Extensions.Configuration.Json.wasm"
+    },
+    {
+      "hash": "sha256-EsqMg6Nn2OmApx+Yh5WkzSz9G6+28J6ihS0FHl/N4ko=",
+      "url": "_framework/Microsoft.Extensions.Configuration.wasm"
+    },
+    {
+      "hash": "sha256-bW/Gfvr6T8HgwG9HabBzLvXLP8hzSg2iIfReZKoobXg=",
+      "url": "_framework/Microsoft.Extensions.DependencyInjection.Abstractions.wasm"
+    },
+    {
+      "hash": "sha256-iSmGmnPMEeFyljZ+UguEzxlcpwPHUHBGRr4SDeT79D4=",
+      "url": "_framework/Microsoft.Extensions.DependencyInjection.wasm"
+    },
+    {
+      "hash": "sha256-NK96RJ5ouvZ2T+H8kea71Ge1SAEZPV1it+EgM/bCr4U=",
+      "url": "_framework/Microsoft.Extensions.FileProviders.Abstractions.wasm"
+    },
+    {
+      "hash": "sha256-+ty/KBBgvbCH74JqV5Pc50oXSfYiaiwGlqxF12+vp8k=",
+      "url": "_framework/Microsoft.Extensions.FileProviders.Physical.wasm"
+    },
+    {
+      "hash": "sha256-RQHYFHnuR8JvJbKDeRSeUtLq+GkdOWB99Gw5HCE7V1g=",
+      "url": "_framework/Microsoft.Extensions.FileSystemGlobbing.wasm"
+    },
+    {
+      "hash": "sha256-ZEyQ/85sEZ1MX0IpiJFHD5geKO7uNyaIgZIfVdwwMg0=",
+      "url": "_framework/Microsoft.Extensions.Hosting.Abstractions.wasm"
+    },
+    {
+      "hash": "sha256-MRqILLS2wBtBn7u5HJlRgR7UY+0InQHQ3FCSMVtaPRQ=",
+      "url": "_framework/Microsoft.Extensions.Logging.Abstractions.wasm"
+    },
+    {
+      "hash": "sha256-gUY3fB6U/YohahZ7IYtwFs4J2u+tdEfdauoptQ605bg=",
+      "url": "_framework/Microsoft.Extensions.Logging.wasm"
+    },
+    {
+      "hash": "sha256-nli8CFjnGGN5ct0I8f3U6bWLAoHqVcP4+LRMxXuDc/8=",
+      "url": "_framework/Microsoft.Extensions.ObjectPool.wasm"
+    },
+    {
+      "hash": "sha256-mngxZ7DtB5tYzmxZdVWZapJTSJ/yGClqQAcqOkLk3Lk=",
+      "url": "_framework/Microsoft.Extensions.Options.wasm"
+    },
+    {
+      "hash": "sha256-i/u1TC8YF4XKf7Uqb843AC1cZVZcsgrpXfZLxG1NTw4=",
+      "url": "_framework/Microsoft.Extensions.Primitives.wasm"
+    },
+    {
+      "hash": "sha256-Jwrlf0VPydOWncfsAvk569TQrosh6ro1Xkc8HzvG2nQ=",
+      "url": "_framework/Microsoft.JSInterop.WebAssembly.wasm"
+    },
+    {
+      "hash": "sha256-/a2ywBVO60H6ZjqSD/C34ynDNuDMyoatYefSNc+dBaU=",
+      "url": "_framework/Microsoft.JSInterop.wasm"
+    },
+    {
+      "hash": "sha256-jfXK+cfJd/PteHB+5Bay/NDY9b9VLvVw6G/Bm262Lpo=",
+      "url": "_framework/Microsoft.Net.Http.Headers.wasm"
+    },
+    {
+      "hash": "sha256-ek7+YNET6EFrzZiiViPooLfQeDdcLjZy0iN/Eh6JXXc=",
+      "url": "_framework/Mobilna_Gra_Terenowa.wasm"
+    },
+    {
+      "hash": "sha256-SCaVX54xcujI1MzS6bdF6durL+jZVRZ4k9kn9g0p0Ic=",
+      "url": "_framework/System.Buffers.wasm"
+    },
+    {
+      "hash": "sha256-qbFVwJ3MwPs4iLA4qWelpJAeIsH2PRjUbI272EIbahg=",
+      "url": "_framework/System.Collections.Concurrent.wasm"
+    },
+    {
+      "hash": "sha256-HAUrTXAQW8eiCUAlUtHCnZj2yaJUyQ1u9weCYgv/8As=",
+      "url": "_framework/System.Collections.Immutable.wasm"
+    },
+    {
+      "hash": "sha256-l+BliKUqDPP+XfoqmJwbygIPJpLOAVoiW1u8a4PWjm8=",
+      "url": "_framework/System.Collections.wasm"
+    },
+    {
+      "hash": "sha256-MNz8G4JyH1jfTZyH22Zc0Cp1mJnJjIchdSnPO09c1rY=",
+      "url": "_framework/System.ComponentModel.Primitives.wasm"
+    },
+    {
+      "hash": "sha256-7/wq7ywD06cnJ8jrbdRfuHvQWIuybuXIxcDyANNExl0=",
+      "url": "_framework/System.ComponentModel.TypeConverter.wasm"
+    },
+    {
+      "hash": "sha256-P2S+kD7qfT2MnIw584jLeOQv/mjAapOZrHTTLVjjLnA=",
+      "url": "_framework/System.ComponentModel.wasm"
+    },
+    {
+      "hash": "sha256-zcck7fgqPSotwUF1KQCfL6/bBhRxtebbPDXy3wlmUsc=",
+      "url": "_framework/System.Console.wasm"
+    },
+    {
+      "hash": "sha256-GxS1Yd3Kc+Eutuk0KgdPfgGRCRcteZLiLA/YDcLgeXY=",
+      "url": "_framework/System.Diagnostics.DiagnosticSource.wasm"
+    },
+    {
+      "hash": "sha256-bBmuoVnwu2lSsxUWw94/lmeBv3CwQO0G5vm9iEsFQhs=",
+      "url": "_framework/System.IO.FileSystem.Watcher.wasm"
+    },
+    {
+      "hash": "sha256-BmnvCS4SDdOFroEmdgT406WwdFRVhd1KAIxlxYEjosw=",
+      "url": "_framework/System.IO.MemoryMappedFiles.wasm"
+    },
+    {
+      "hash": "sha256-cK30UWG18uD2MKlwqM8nYDkq6lARekPhwtH4ISQy+nc=",
+      "url": "_framework/System.Linq.Expressions.wasm"
+    },
+    {
+      "hash": "sha256-QVS3tIFO7/w4oPraqlTIdmw1y4RLZyo2jaLjNnCpeJc=",
+      "url": "_framework/System.Linq.wasm"
+    },
+    {
+      "hash": "sha256-huFCeZesp5+FPHzvKrbfloYQjicXCMuAA6kMcwv88/k=",
+      "url": "_framework/System.Memory.wasm"
+    },
+    {
+      "hash": "sha256-5lfbbAtzMh+2qphHXMXaUGoLo0hRK2inxRs5Ybkp0Ec=",
+      "url": "_framework/System.Net.Http.wasm"
+    },
+    {
+      "hash": "sha256-Lu1TnTM2YWedDDCt7bCtjEDwDLD3e1pD3sUM7wWFQp0=",
+      "url": "_framework/System.Net.Primitives.wasm"
+    },
+    {
+      "hash": "sha256-oB2I1xs2FDuvxPBikqT/bI/n5s2iqK4Wc5h+A9ab7jQ=",
+      "url": "_framework/System.Net.WebSockets.wasm"
+    },
+    {
+      "hash": "sha256-/LtMMvl9x7SJMbG3GzwSVpZWfv0QrZJv5A0LRCzCi+w=",
+      "url": "_framework/System.ObjectModel.wasm"
+    },
+    {
+      "hash": "sha256-Xzcd0u8UGPqz1lPZAjDHhc8yvHc2UUBTzMUjYFmiYLc=",
+      "url": "_framework/System.Private.CoreLib.wasm"
+    },
+    {
+      "hash": "sha256-5aRlPyDoKhYO6eb2h0joaU9Gib9lZGWgxWqvdsS/P2w=",
+      "url": "_framework/System.Private.Uri.wasm"
+    },
+    {
+      "hash": "sha256-tc2+H0cOBCK5BVr1sXFZcohROeK3EwUk3ICVR9x22ks=",
+      "url": "_framework/System.Reflection.Metadata.wasm"
+    },
+    {
+      "hash": "sha256-xXX57WWAM8npFIs6tQueqpMQ1sjXV+pl1QyUTn0udc4=",
+      "url": "_framework/System.Runtime.InteropServices.JavaScript.wasm"
+    },
+    {
+      "hash": "sha256-Wt0Y57wFUlvum/BOEt50J74BfmMpfzzk/+35GzcmsZ8=",
+      "url": "_framework/System.Runtime.wasm"
+    },
+    {
+      "hash": "sha256-Kv2p4TTfg9/BhT8lB/nehcRI/NNPNhoZTRB8JhWRVOw=",
+      "url": "_framework/System.Security.Claims.wasm"
+    },
+    {
+      "hash": "sha256-j0P6AUV1s5wiYQYAPQfyP0E9VJelHjUCu2WInLp/QB0=",
+      "url": "_framework/System.Security.Cryptography.wasm"
+    },
+    {
+      "hash": "sha256-Y1hFfyqkGfmrE7qWmy5B7WnIN71BP9EOWdbaSMJNZ5o=",
+      "url": "_framework/System.Text.Encodings.Web.wasm"
+    },
+    {
+      "hash": "sha256-5RRtg8iwxv2q5P+k1gAfsnT7ICjHRHeL8yVTFjLJrec=",
+      "url": "_framework/System.Text.Json.wasm"
+    },
+    {
+      "hash": "sha256-qSNlP6rmJ/Kr+CygGHYytUckhzcBVHfyUqEhCXRK/r8=",
+      "url": "_framework/System.Text.RegularExpressions.wasm"
+    },
+    {
+      "hash": "sha256-YrP+GnlZXZy7ejaNg/du0etF6XXCQHFnruCL15ELESU=",
+      "url": "_framework/blazor.boot.json"
+    },
+    {
+      "hash": "sha256-gvHfnndfEu1tRf0rFb5988rWq7ITIotOaE8+AMbKYbc=",
+      "url": "_framework/blazor.webassembly.js"
+    },
+    {
+      "hash": "sha256-FXdaLA6xjaMDqi/Wc6ytVGOY0dVgPsSsY9/14W3nZSQ=",
+      "url": "_framework/dotnet.js"
+    },
+    {
+      "hash": "sha256-B6DNRvFeCUi3jbx9uKpuj+hV9cN0KG4RdZj1FgBZwj8=",
+      "url": "_framework/dotnet.native.js"
+    },
+    {
+      "hash": "sha256-4X9eav+Ak45oe3jDiM7H2gSxg1ZRzmhn9QfZURenUbE=",
+      "url": "_framework/dotnet.native.wasm"
+    },
+    {
+      "hash": "sha256-aTAnT1r/EAWev8oI/mTFI9nH3RsfA5M822bNiGhok6U=",
+      "url": "_framework/dotnet.runtime.js"
+    },
+    {
+      "hash": "sha256-SZLtQnRc0JkwqHab0VUVP7T3uBPSeYzxzDnpxPpUnHk=",
+      "url": "_framework/icudt_CJK.dat"
+    },
+    {
+      "hash": "sha256-8fItetYY8kQ0ww6oxwTLiT3oXlBwHKumbeP2pRF4yTc=",
+      "url": "_framework/icudt_EFIGS.dat"
+    },
+    {
+      "hash": "sha256-L7sV7NEYP37/Qr2FPCePo5cJqRgTXRwGHuwF5Q+0Nfs=",
+      "url": "_framework/icudt_no_CJK.dat"
+    },
+    {
+      "hash": "sha256-Ymt4H/EFVTkKOf6E/cSQqNJshexHxkONN0K+vGi+56I=",
+      "url": "_framework/netstandard.wasm"
+    },
+    {
+      "hash": "sha256-7LQwK5J39tvdUVtI/QR1MxQfU4FENmSDgekv9iZ093c=",
+      "url": "css/app.css"
+    },
+    {
+      "hash": "sha256-z8OR40MowJ8GgK6P89Y+hiJK5+cclzFHzLhFQLL92bg=",
+      "url": "css/bootstrap/bootstrap.min.css"
+    },
+    {
+      "hash": "sha256-gBwg2tmA0Ci2u54gMF1jNCVku6vznarkLS6D76htNNQ=",
+      "url": "css/bootstrap/bootstrap.min.css.map"
+    },
+    {
+      "hash": "sha256-4mWsDy3aHl36ZbGt8zByK7Pvd4kRUoNgTYzRnwmPHwg=",
+      "url": "favicon.png"
+    },
+    {
+      "hash": "sha256-DbpQaq68ZSb5IoPosBErM1QWBfsbTxpJqhU0REi6wP4=",
+      "url": "icon-192.png"
+    },
+    {
+      "hash": "sha256-oEo6d+KqX5fjxTiZk/w9NB3Mi0+ycS5yLwCKwr4IkbA=",
+      "url": "icon-512.png"
+    },
+    {
+      "hash": "sha256-eBDLkcUCoScRyVkKzZOsFF3WDC7F7gKmY2GPA/mSsRM=",
+      "url": "index.html"
+    },
+    {
+      "hash": "sha256-ZLtP6up4lHsXulHFHGtTIknEWD6vPPJgfYjvZdkKy20=",
+      "url": "manifest.webmanifest"
+    },
+    {
+      "hash": "sha256-cPA4UofqCHy4I4o+dNSviBdHPcdERUTk4R9wm7NsKt0=",
+      "url": "scripts/map.js"
+    }
+  ]
+};
